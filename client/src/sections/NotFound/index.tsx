c158cd2cd33664994b92e9b1fac3e73b5a7cabe0
Kenechukwu <kenechukwu.ajufo@gmail.com>
2022-06-24 15:02:20 +0100

    Added UI For Not Found Route

--- client/src/sections/NotFound/index.tsx
+++ client/src/sections/NotFound/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { Empty, Layout, Typography } from "antd";
+import { Link } from 'react-router-dom';
+
+const { Content } = Layout;
+const { Text } = Typography;
+
 
 export const NotFound = () => {
-    return <h2>NotFound</h2>
+    return (
+        <Content className='not-found'>
+            <Empty description= {
+                <>
+                    <Text className='not-found__description-title'>Uh Oh! Something went wrong :( </Text>
+                    <Text className='not-found__description-subtitle'>The page you are looking for cannot be found.</Text>
+                </>
+            }
+            />
+            <Link to="/" className='not-found__cta ant-btn ant-btn-primary ant-btn-lg'>Go to Home</Link>
+        </Content>
+    )
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-require("dotenv").config();
+//require("dotenv").config();
 
 import express, { Application } from "express";
 import bodyParser from "body-parser";
@@ -6,12 +6,17 @@ import cookieParser from "cookie-parser";
 import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./database";
 import { typeDefs, resolvers } from "./graphql";
+import compression from "compression";
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
 
   app.use(bodyParser.json({ limit: "2mb" }));
   app.use(cookieParser(process.env.SECRET));
+  app.use(compression());
+
+  app.use(express.static(`${__dirname}/client`));
+  app.get("/*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
 
   const server = new ApolloServer({
     typeDefs,
